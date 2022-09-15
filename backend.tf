b9308e03104e362b35c2b99c3d161ec704e7d011
Kapil <kapil.kumaria@gmail.com>
2022-09-15 15:45:04 +0000

    fixed remote file's paths

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state-kk15"
-    key            = "global/terraform/terraform.tfstate"
+    bucket = "terraform-up-and-running-state-kk15"
+    key    = "global/terraform/terraform.tfstate"
     #key            = "terraform"
-    region         = "us-east-1"
+    region = "us-east-1"
     # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
--- eip.tf
+++ eip.tf
@@ -1,10 +1,5 @@
-provider "aws" {
-    profile = "MyAWS"
-    region = "us-east-1"
-}
-
 resource "aws_eip" "eip" {
-    tags = {
-        Name = "projecteip1"
-    }
+  tags = {
+    Name = "projecteip1"
+  }
 }
